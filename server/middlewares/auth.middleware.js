a6617103371099bdbf173d71969661b4dd592d3e
Vivek <vivekparde2411@gmail.com>
2023-08-06 18:01:51 +0530

    created authorizedRoles middleware

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
 import AppError from "../utils/error.util.js";
 import jwt from "jsonwebtoken";
 
-const isLoggedIn = async (req, res, next)=>{
-    const {token} = req.cookies;
+const isLoggedIn = async (req, res, next) => {
+    const { token } = req.cookies;
 
     if (!token) {
         return next(new AppError('Unauthenticated, please login', 400))
@@ -13,4 +13,12 @@ const isLoggedIn = async (req, res, next)=>{
     req.user = userDetail;
     next();
 }
-export {isLoggedIn}
+
+const authorizedRoles = (...roles) => async (req, res, next) => {
+    const currentUserRole = req.user.role;
+    if (!roles.includes(currentUserRole)) {
+        return next(new AppError('You do not have permission to access this route', 403));
+    }
+    next()
+}
+export { isLoggedIn, authorizedRoles }
