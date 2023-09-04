bb8f47e9394fa612fab37f913ff61ecd743013f4
Vivek <vivekparde2411@gmail.com>
2023-09-04 14:29:40 +0530

    created basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <button>Click me</button>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+import ReactDOM from 'react-dom/client';
+import {Toaster} from 'react-hot-toast'
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+    <Toaster />
+  </BrowserRouter>
+)
