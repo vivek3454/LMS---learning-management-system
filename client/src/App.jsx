7ee097e0fa1791ffe1f8474fbdac7099d7bc6fc7
Vivek <vivekparde2411@gmail.com>
2023-09-14 14:09:30 +0530

    added require auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import {Route,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
+import RequireAuth from "./Components/Auth/RequireAuth";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import CourseDescription from "./Pages/Course/CourseDescription";
 import CourseList from "./Pages/Course/CourseList";
+import CreateCourse from "./Pages/Course/CreateCourse";
 import Denied from "./Pages/Denied";
 import HomePage from "./Pages/HomePage";
 import Login from "./Pages/Login";
@@ -13,21 +15,25 @@ function App() {
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
 
-      <Route path="/denied" element={<Denied/>}/>
+        <Route path="/denied" element={<Denied />} />
 
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/login" element={<Login/>}/>
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+          <Route path="/course/create" element={<CreateCourse />} />
+        </Route>
 
-      <Route path="/courses" element={<CourseList/>}/>
-      <Route path="/course/description" element={<CourseDescription/>}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="*" element={<NotFound/>}/>
-    </Routes>
+        <Route path="/courses" element={<CourseList />} />
+        <Route path="/course/description" element={<CourseDescription />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
