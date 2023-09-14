8094df1be0b6a79018571861d79e90fdfb697091
Vivek <vivekparde2411@gmail.com>
2023-09-14 15:18:14 +0530

    added course creation by admin

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,10 +1,10 @@
-import { AiFillCloseCircle } from 'react-icons/ai'
-import { FiMenu } from 'react-icons/fi'
+import { AiFillCloseCircle } from "react-icons/ai";
+import { FiMenu } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Footer from '../Components/Footer'
-import { logout } from '../Redux/Slices/AuthSlice';
+import Footer from "../Components/Footer";
+import { logout } from "../Redux/Slices/AuthSlice";
 
 const HomeLayout = ({ children }) => {
     const dispatch = useDispatch();
@@ -30,15 +30,15 @@ const HomeLayout = ({ children }) => {
     // open side bar
     const openDrawer = () => {
         const drawerSide = document.getElementsByClassName("drawer-overlay");
-        drawerSide[0].style.width = 'auto';
-    }
+        drawerSide[0].style.width = "auto";
+    };
     // hide side bar
     const hideDrawer = () => {
         const drawerToggle = document.getElementsByClassName("drawer-toggle");
         drawerToggle[0].checked = false;
         const drawerSide = document.getElementsByClassName("drawer-overlay");
         drawerSide[0].style.width = 0;
-    }
+    };
     return (
         <>
             <div className="min-h-[90vh]">
@@ -49,14 +49,14 @@ const HomeLayout = ({ children }) => {
                             <FiMenu
                                 onClick={openDrawer}
                                 size={"32px"}
-                                className='font-bold text-white m-4'
+                                className="font-bold text-white m-4"
                             />
                         </label>
                     </div>
                     <div className="drawer-side">
-                        <label htmlFor="my-drawer" className='drawer-overlay'></label>
+                        <label htmlFor="my-drawer" className="drawer-overlay"></label>
                         <ul className="menu p-4 w-48 sm:w-80 bg-base-200 h-full text-base-content relative">
-                            <li className='w-fit absolute right-2 z-50'>
+                            <li className="w-fit absolute right-2 z-50">
                                 <button onClick={hideDrawer}>
                                     <AiFillCloseCircle
                                         size={24}
@@ -64,7 +64,7 @@ const HomeLayout = ({ children }) => {
                                 </button>
                             </li>
                             <li>
-                                <Link to={'/'}>Home</Link>
+                                <Link to={"/"}>Home</Link>
                             </li>
                             {/* displaying dashboard, if user is logged in */}
                             {isLoggedIn && role === "ADMIN" && (
@@ -72,14 +72,19 @@ const HomeLayout = ({ children }) => {
                                     <Link to={"/admin/dashboard"}>Admin Dashboard</Link>
                                 </li>
                             )}
+                            {isLoggedIn && role === "ADMIN" && (
+                                <li>
+                                    <Link to={"/course/create"}>Create Course</Link>
+                                </li>
+                            )}
                             <li>
-                                <Link to={'/courses'}>All Courses</Link>
+                                <Link to={"/courses"}>All Courses</Link>
                             </li>
                             <li>
-                                <Link to={'/contact'}>Contact Us</Link>
+                                <Link to={"/contact"}>Contact Us</Link>
                             </li>
                             <li>
-                                <Link to={'/about'}>About Us</Link>
+                                <Link to={"/about"}>About Us</Link>
                             </li>
                             {/* creating the bottom part of drawer */}
                             {/* if user is not logged in */}
@@ -116,7 +121,7 @@ const HomeLayout = ({ children }) => {
                 <Footer />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default HomeLayout
+export default HomeLayout;
