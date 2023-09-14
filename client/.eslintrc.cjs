a364b02201869358adf8806e9fbbc3bc1dc14307
Vivek <vivekparde2411@gmail.com>
2023-09-14 15:19:33 +0530

    added eslint rule

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,20 +3,23 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'simple-import-sort'],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
-    'simple-import-sort/imports': 'error',
+    "simple-import-sort/imports": "error",
     "react/prop-types": "off",
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "quotes": [2, "double", { "avoidEscape": true }],
+    "semi": [2, "always"],
+    "jsx-quotes": ["error", "prefer-double"]
   },
-}
+};
